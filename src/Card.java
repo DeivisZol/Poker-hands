2ca416c1b53cfb470bc8950038ec0b04df3300ee
DeivisZol <68916713+DeivisZol@users.noreply.github.com>
2022-01-25 16:29:54 +0200

    Card class
    created two enums that represent card

---
+++ src/Card.java
@@ -0,0 +1,7 @@
+public class Card {
+    private CardType cardType;
+    private CardClass cardClass;
+
+    void Card(String card) {
+    }
+}
---
+++ src/CardType.java
@@ -0,0 +1,6 @@
+public enum CardType {
+    Diamonds,
+    Clubs,
+    Hearths,
+    Spades
+}
